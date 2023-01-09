1cd605158349c7fd88fdd73eb6c66f643684d443
Daniel Drabik <dan.drabik@gmail.com>
2023-01-09 11:43:40 -0500

    Working version of hitting endpoint and show/hide (token redacted).

--- app/static/script.js
+++ app/static/script.js
@@ -8,14 +8,21 @@ window.addEventListener('load', function () {
   const wordCountField = document.getElementById('js-wordCount');
   const countersBox = document.getElementById('js-counters');
 
+  const feedbackBoxes = document.getElementsByClassName("js-feedback");
   const warningBox = document.getElementById('js-warning');
   const processingBox = document.getElementById('js-processing');
   const humanBox = document.getElementById('js-human');
   const aiBox = document.getElementById('js-ai');
-  const minWords = 100;
+
+
+  const minWords = 5;
   const maxWords = 400;
+  const endpoint = 'https://us-central1-aiplatform.googleapis.com/v1/projects/gpt-experiments-373801/locations/us-central1/endpoints/2125791383095607296:predict'
   const errorClass = 'errorBox';
   const validClass = 'validBox';
+  const hideClass = 'hide';
+  const humanLabel = 'Real';
+  const aiLabel = 'Fake';
 
   form.addEventListener('submit', handleSubmit);
   textBox.addEventListener('input', handleTyping);
@@ -33,9 +40,9 @@ window.addEventListener('load', function () {
     markValid();
 
     console.log('Submit form');
-    // TODO finish form submit
-    // show approriate box based on status.
 
+    showBox(processingBox);
+    fetchAndUpdate(textBox.value);
   }
 
   function handleTyping(event) {
@@ -56,15 +63,11 @@ window.addEventListener('load', function () {
   }
 
   function countValid(count) {
-    return (count > minWords || count >= maxWords);
+    return (count >= minWords || count >= maxWords);
   }
 
   function wordCount() {
     return textBox.value.split(" ").length;
-  }
-
-  function showBox(box) {
-
   }
 
   function markValid() {
@@ -79,5 +82,50 @@ window.addEventListener('load', function () {
     submitButton.disabled = true;
   }
 
+  function fetchAndUpdate(words) {
+    postData(endpoint, { "instances": [{"data": words}]})
+      .then((data) => {
+        console.log(data); // JSON data parsed by `data.json()` call
+        const result = data.predictions[0];
+        const label = result.label;
+
+        if (label == humanLabel) {
+          showBox(humanBox);
+        }
+
+        if (label == aiLabel) {
+          showBox(aiBox);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  function hideAllBoxes() {
+    Array.from(feedbackBoxes).forEach((el) => {
+      el.classList.add(hideClass);
+    })
+  }
+
+  function showBox(box) {
+    hideAllBoxes();
+    box.classList.remove(hideClass)
+  }
+
+  async function postData(url = '', data = {}) {
+    const token = ""
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      },
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  }
 });
 
