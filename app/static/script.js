ad52e17e0c350b2208771292bff6cfb21a1ee89d
dandrabik <dandrabik@users.noreply.github.com>
2023-01-09 17:34:23 -0500

    UI updates (#1)
    * Move current index to other page.
    
    * Initial html
    
    * Add some more style.
    
    * Reasonably accurate layout.
    
    * Add icon and links
    
    * Add message boxes.
    
    * Point to blank script.
    
    * Add basic javascript for validating input.
    
    * Add todos for Monday.
    
    * Add missing div.
    
    * Working version of hitting endpoint and show/hide (token redacted).
    
    * Remove comments.
    
    * Point to classify endpoint.

--- app/static/script.js
+++ app/static/script.js
@@ -2,6 +2,129 @@
 
 window.addEventListener('load', function () {
 
-  console.log("Hello World!");
+  const textBox = document.getElementById('js-textBox');
+  const form = document.getElementById('js-form');
+  const submitButton = document.getElementById('js-submitButton');
+  const wordCountField = document.getElementById('js-wordCount');
+  const countersBox = document.getElementById('js-counters');
 
+  const feedbackBoxes = document.getElementsByClassName("js-feedback");
+  const warningBox = document.getElementById('js-warning');
+  const processingBox = document.getElementById('js-processing');
+  const humanBox = document.getElementById('js-human');
+  const aiBox = document.getElementById('js-ai');
+
+  const minWords = 5;
+  const maxWords = 400;
+  const endpoint = '/classify'
+  const errorClass = 'errorBox';
+  const validClass = 'validBox';
+  const hideClass = 'hide';
+  const humanLabel = 'Real';
+  const aiLabel = 'Fake';
+
+  form.addEventListener('submit', handleSubmit);
+  textBox.addEventListener('input', handleTyping);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const count = wordCount();
+
+    if (!countValid(count)) {
+      markInvalid();
+      return
+    }
+
+    markValid();
+
+    console.log('Submit form');
+
+    showBox(processingBox);
+    fetchAndUpdate(textBox.value);
+  }
+
+  function handleTyping(event) {
+
+    const count = wordCount();
+
+    wordCountField.innerHTML = count;
+
+    if (count >= maxWords) {
+      markInvalid()
+      return;
+    }
+
+    if (countValid(count)) {
+      markValid();
+      return;
+    }
+  }
+
+  function countValid(count) {
+    return (count >= minWords || count >= maxWords);
+  }
+
+  function wordCount() {
+    return textBox.value.split(" ").length;
+  }
+
+  function markValid() {
+    countersBox.classList.remove(errorClass);
+    countersBox.classList.add(validClass);
+    submitButton.disabled = false;
+  }
+
+  function markInvalid() {
+    countersBox.classList.add(errorClass);
+    countersBox.classList.remove(validClass);
+    submitButton.disabled = true;
+  }
+
+  function fetchAndUpdate(words) {
+    postData(endpoint, { "instances": [{"data": words}]})
+      .then((data) => {
+        console.log(data); // JSON data parsed by `data.json()` call
+        const result = data.predictions[0];
+        const label = result.label;
+
+        if (label == humanLabel) {
+          showBox(humanBox);
+        }
+
+        if (label == aiLabel) {
+          showBox(aiBox);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  function hideAllBoxes() {
+    Array.from(feedbackBoxes).forEach((el) => {
+      el.classList.add(hideClass);
+    })
+  }
+
+  function showBox(box) {
+    hideAllBoxes();
+    box.classList.remove(hideClass)
+  }
+
+  async function postData(url = '', data = {}) {
+    const token = ""
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      },
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer',
+      body: JSON.stringify(data)
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  }
 });
+
